3465750396b0dd0857e4d9db66d9f4654ab9cd04
Raul <raulsanc@student.42malaga.com>
2025-09-25 14:58:38 +0200

    exam

--- Nvl3/rstr_capitalizer/rstr_capitalizer.c
+++ Nvl3/rstr_capitalizer/rstr_capitalizer.c
@@ -1,44 +1,31 @@
 #include <unistd.h>
 
-void put_char(char c)
-{
-	write(1, &c, 1);
-}
-
-int is_space(char c)
-{
-	if(c <= 32)
-		return (1);
-	return (0);	
-}
-
-void r_capitalizer(char *s)
+void  rstr_capitalizer(char *str)
 {
     int i = 0;
-
-    while (s[i])
+    while (str[i])
     {
-         if (s[i] >= 'A' && s[i] <= 'Z')
-             s[i] += 32;
-         if ((s[i] >= 'a' && s[i] <= 'z') && is_space(s[i + 1]))
-             s[i] -= 32;
-         put_char(s[i++]);
+        // Poner en minúscula cualquier letra mayúscula
+        if (str[i] >= 'A' && str[i] <= 'Z')
+            str[i] += 32;
+        // Si es letra y el siguiente es separador (espacio, tab) o fin de cadena, capitalizar
+        if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == ' ' || str[i + 1] == '\t' || str[i + 1] == '\0'))
+            str[i] -= 32;
+        write(1, &str[i], 1);
+        i++;
     }
 }
 
-int main (int ac, char** av)
+int main(int argc, char **argv)
 {
-	int i = 1;
-
-	if(ac == 1)
-		put_char('\n');
-	else
+	int	i = 1;
+	
+	if (argc == 1)
+		write(1, "\n", 1);
+	while (argc > i)
 	{
-		while(i < ac)
-		{
-			r_capitalizer(av[i]);
-			put_char('\n');
-			i++;
-		}
+		rstr_capitalizer(argv[i]);
+		write(1, "\n", 1);
+		i++;
 	}
 }
