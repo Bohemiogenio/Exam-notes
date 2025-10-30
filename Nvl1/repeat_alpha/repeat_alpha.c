c8232b579a7db2ba6ff0c61b49fb8ad92bb7336c
Bohemio <you@example.com>
2025-10-30 15:38:58 +0100

    exam

--- Nvl1/repeat_alpha/repeat_alpha.c
+++ Nvl1/repeat_alpha/repeat_alpha.c
@@ -1,44 +1,36 @@
 #include <unistd.h>
-i
-int main(int argc , char **argv)
-{
-	int i;
-	int j;
-	
-	if (argc == 2) /* argc == 2 → significa que el usuario pasó 1 argumento
-			  además del nombre del programa */
-	{	
-		i = 0;
 
-		while (argv[1][i]) /*recorre el argumento*/
+int main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		int i = 0;
+		int j = 0;
+		while (av[1][i])
 		{
-			j = 0;
-			if (argv[1][i] >= 65 && argv[1][i] <= 90) /*si es mayuscula entra aqui*/
-			{	
-				while (j < argv[1][i] - 64)
-				{
-					write(1, &argv[1][i], 1);
-					j++;
-				}	
-					/*Si le pasamos el valor de C que en codigo ascii es de 67
-					 * este bucle hace que reste 67 - 64 = 3, y como j < 3
-					 * entra 3 veces en el bucle e imprime 3 letras C */
-				
-			}
-			else if (argv[1][i] >= 97 && argv[1][i] <= 122) /*si es minuscula*/
+			if (av[1][i] >= 'a' && av[1][i] <= 'z')
 			{
-				while (j < argv[1][i] - 96)
+				j = 0;
+				while(j < av[1][i] - 96)
 				{
-					write(1, &argv[1][i], 1);
-					j++;
+					write(1, &av[1][i], 1);
+				j++;
 				}
 			}
+			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+			{
+				j = 0;
+				while(j < av[1][i] - 64)
+                                {
+                                        write(1, &av[1][i], 1);
+                                j++;
+                                }
+			}
 			else
-				write(1, &argv[1][i], 1); /* Que imprima en pantalla todo lo que
-							     no sea letras*/
-			i++;
-		}
+				 write(1, &av[1][i], 1);
+		i++;
+		}	
 	}
-	write(1, "\n", 1); /*salto de linea final*/
-	return 0;
+	write(1, "\n", 1);
+	return (0);
 }
