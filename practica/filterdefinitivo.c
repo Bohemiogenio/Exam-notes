24abf31dd0735b0e45a684b629524e1dc302ef85
Bohemio <you@example.com>
2025-12-11 20:17:53 +0100

    exam

---
+++ practica/filterdefinitivo.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int ft_strncmp(char *s1, char *s2, int size)
+{
+	int i = 0;
+
+	while (s1[i] && s2[i] && i < size)
+	{
+		if (s1[i] != s2[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int main(int ac, char **av)
+{
+	int	i;
+	int	size;
+	int	b_read;
+	char	buf[9999];
+	char	c;
+
+	i = 0;
+	if (ac != 2 || av[1] == NULL)
+		return (1);
+	size = strlen (av[1]);
+	while((b_read = read(0, &c, 1)) > 0)
+	{
+		if (b_read < 0)
+		{
+			fprintf(stderr, "Error: ");
+			return (1);
+		}
+		buf[i] = c;
+		i++;
+	}
+	i = 0;
+	while (buf[i])
+	{
+		if (ft_strncmp(&buf[i], av[1], size))
+		{
+			write(1, &buf[i], 1);
+			i++;
+		}
+		else
+		{
+			for (int j = 0; j < size; j++)
+				write(1, "*", 1);
+			i += size;
+		}
+	}
+	return (0);
+}
+
